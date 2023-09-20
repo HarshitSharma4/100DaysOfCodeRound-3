7d369cdb23d1cf530b64236af4b212fdc4711cb3
unique011 <harshitsharma.2021cs@technonjr.org>
2023-09-20 21:40:43 +0530

    100-Days-Of-Code-Round-3

--- Photo_Album_material_UI/src/components/NavigationBar3.jsx
+++ Photo_Album_material_UI/src/components/NavigationBar3.jsx
@@ -1,5 +1,65 @@
+import {
+  AccountCircleRounded,
+  MailLock,
+  Notifications,
+  Search,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import "../index.css";
 const NavigationBar3 = () => {
-  return <div></div>;
+  return (
+    <AppBar position="relative">
+      <Toolbar
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="h4" component="h4">
+          Lama <span className="hide">dev</span>
+        </Typography>
+        <Box
+          style={{
+            width: "40%",
+            backgroundColor: "#989c9fcc",
+            height: "37px",
+            display: "flex",
+            borderRadius: "10px",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "1.5rem",
+          }}
+        >
+          <Search style={{ marginLeft: "10px" }} />
+          <InputBase placeholder="Search..." />
+        </Box>
+        <div>
+          <IconButton>
+            <Badge badgeContent={4} color="secondary">
+              <MailLock />
+            </Badge>
+          </IconButton>
+          <IconButton>
+            <Badge badgeContent={7} color="secondary">
+              <Notifications />
+            </Badge>
+          </IconButton>
+          <IconButton>
+            <AccountCircleRounded />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavigationBar3;
--- Photo_Album_material_UI/src/pages/Page3.jsx
+++ Photo_Album_material_UI/src/pages/Page3.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 
+import NavigationBar3 from "../components/NavigationBar3";
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      light: "#1F75FE",
+      main: "#1F75FE",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
 const Page3 = () => {
-  return <div></div>;
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <NavigationBar3 />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Page3;
